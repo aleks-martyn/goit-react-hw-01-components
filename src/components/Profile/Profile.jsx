ae30e297120fcd5118990ed591dc29ff62ec4ae3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-25 19:24:38 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
 export const Profile = ({
   username,
@@ -10,30 +11,30 @@ export const Profile = ({
   likes,
 }) => {
   return (
-    <div class="profile">
-      <div class="description">
+    <div className={css.profile}>
+          <div className={css.description}>
         <img
           src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          class="avatar"
+          alt={username}
+          className={css.avatar}
         />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+        <p className={css.name}>{username}</p>
+              <p className={css.tag}>{tag }</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul class="stats">
+      <ul className={css.stats}>
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
