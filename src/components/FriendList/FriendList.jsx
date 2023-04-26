66e86d44f48389c89c7b66a732cefd84390f2f8d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-27 00:22:02 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 import css from './FriendList.module.css';
 
 export const FriendList = ({ friends }) => {
   return (
     <ul className={css.friend_list}>
       {friends.map(({ id, avatar, name, isOnline }) => (
-        <li key={id} avatar={avatar} name={name} isOnline={isOnline} />
+        <FriendListItem key={id} avatar={avatar} name={name} isOnline={isOnline} />
       ))}
     </ul>
   );
