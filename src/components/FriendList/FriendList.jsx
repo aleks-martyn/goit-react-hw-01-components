f533d105c7f51ab09f5c919f3926d8c3ed26e608
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-26 18:34:18 +0300

    Update FriendList.jsx

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friend_list}>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <li key={id} avatar={avatar} name={name} isOnline={isOnline} />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
