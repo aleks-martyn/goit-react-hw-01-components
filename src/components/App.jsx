fc5979f788eb02fbc624bd57a120e7199b3c6fd6
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-26 18:34:07 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 import user from 'user.json';
 import data from 'data.json';
+import friends from 'friends.json';
 
 export const App = () => {
   const {
@@ -24,6 +26,7 @@ export const App = () => {
         likes={likes}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
