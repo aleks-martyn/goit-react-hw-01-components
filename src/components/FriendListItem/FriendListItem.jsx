8fb958e099087f7c4e68c041dcff8a5a9cc88b1d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-27 01:12:05 +0300

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,7 +4,7 @@ import css from './FriendListItem.module.css';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className={css.status}></span>
+      <span className={css.status}>{isOnline}</span>
       <img className={css.avatar} src={avatar} alt={name} width={48} />
       <p className={css.name}>{name}</p>
     </li>
