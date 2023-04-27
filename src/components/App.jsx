94fee4cebb74e10a1f49fc2a4d880b35b2a1017e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-27 15:33:35 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
-import user from 'user.json';
-import data from 'data.json';
-import friends from 'friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
   const {
@@ -27,6 +29,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
