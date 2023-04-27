777495615448af6bd7884d25fbecc816a949b609
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-28 00:24:40 +0300

    Update Statistics.styled.jsx

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,6 +1,18 @@
 import styled from '@emotion/styled';
 
+export const Section = styled.section`
+  width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
+  text-align: center;
+  background-color: #fff;
+`;
+
 export const SectionTitle = styled.h2`
+  margin: 0;
+  padding-top: 20px;
+  padding-bottom: 20px;
   font-size: 20px;
   text-transform: uppercase;
   color: #424949;
@@ -8,9 +20,22 @@ export const SectionTitle = styled.h2`
 
 export const StatList = styled.ul`
   display: flex;
+  padding: 0;
+  margin: 0;
   list-style-type: none;
 `;
 
 export const StatListItem = styled.li`
   display: flex;
+  flex-direction: column;
+`;
+
+export const ItemLabel = styled.span`
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+export const ItemPercentage = styled.span`
+  padding-top: 10px;
+  padding-bottom: 10px;
 `;
