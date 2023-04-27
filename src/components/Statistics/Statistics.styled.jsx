046d4806cffccce3786d74e6ca179d880f30a76c
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-27 22:48:39 +0300

    Update Statistics.styled.jsx

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const SectionTitle = styled.h2`
+  font-size: 20px;
+  text-transform: uppercase;
+  color: #424949;
+`;
+
+export const StatList = styled.ul`
+  display: flex;
+  list-style-type: none;
+`;
+
+export const StatListItem = styled.li`
+  display: flex;
+`;
