e86738d35a68cb49c811bd8d55ce37735fad573d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-27 20:12:35 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Profile.module.css';
+import { UserCard, Description, UserImage, UserName, Stats } from './Profile.styled';
 
 export const Profile = ({
   username,
@@ -11,15 +12,15 @@ export const Profile = ({
   likes,
 }) => {
   return (
-    <div className={css.profile}>
-      <div className={css.description}>
-        <img src={avatar} alt={username} className={css.avatar} />
-        <p className={css.name}>{username}</p>
+    <UserCard>
+      <Description>
+        <UserImage src={avatar} alt={username} />
+        <UserName>{username}</UserName>
         <p className={css.tag}>{tag}</p>
         <p className={css.location}>{location}</p>
-      </div>
+      </Description>
 
-      <ul className={css.stats}>
+      <Stats>
         <li>
           <span className={css.label}>Followers</span>
           <span className={css.quantity}>{followers}</span>
@@ -32,8 +33,8 @@ export const Profile = ({
           <span className={css.label}>Likes</span>
           <span className={css.quantity}>{likes}</span>
         </li>
-      </ul>
-    </div>
+      </Stats>
+    </UserCard>
   );
 };
 
