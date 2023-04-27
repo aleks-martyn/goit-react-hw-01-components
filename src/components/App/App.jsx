d4cf02691b87eb8c2b92a4ad97df6e6302c1ce44
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-27 18:25:19 +0300

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,36 @@
+import { Profile } from '../Profile/Profile';
+import { Statistics } from '../Statistics/Statistics';
+import { FriendList } from '../FriendList/FriendList';
+import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
+import user from '../Profile/user.json';
+import data from '../Statistics/data.json';
+import friends from '../FriendList/friends.json';
+import transactions from '../TransactionHistory/transactions.json';
+import { Container } from './App.styled';
+
+export const App = () => {
+  const {
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  } = user;
+
+  return (
+    <Container>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        followers={followers}
+        views={views}
+        likes={likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
+  );
+};
