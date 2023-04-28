014a51c9c87bc47ccc6f7eb1a669b10755b3dff3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-28 14:27:11 +0300

    Update FriendListItem.styled.jsx

---
+++ src/components/FriendListItem/FriendListItem.styled.jsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const ListItem = styled.li`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  width: 380px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
+    0px 2px 1px rgba(0, 0, 0, 0.2);
+`;
+
+export const ItemStatus = styled.span`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: green;
+`;
+
+export const ItemImage = styled.img`
+  width: 60px;
+  background-color: #ececec;
+  border-radius: 5px;
+`;
+
+export const ItemName = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+`;
