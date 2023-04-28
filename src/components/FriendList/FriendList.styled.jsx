ff8e937eb7e535ff669342d47fe7f43190f8af72
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-04-29 01:18:26 +0300

    Update FriendList.styled.jsx

--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -1,9 +1,6 @@
 import styled from '@emotion/styled';
 
 export const ListOfFriends = styled.ul`
-  padding: 0;
-  margin: 0;
   margin-bottom: 40px;
-  list-style-type: none;
   text-align: center;
 `;
